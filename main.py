f060a210cbfa999f71961bdaf3d992185a46d3fc
Kyla Sauber <kylafaithsauber@gmail.com>
2026-03-18 10:06:16 -0400

    3/18/26 10:06

---
+++ main.py
@@ -0,0 +1,6 @@
+prefixes = [
+    {"name": "acorn", 
+    "traits": ["nervous", "childish", "insecure", "calm", "careful", "faithful", "loving", "oblivious"]
+    "colors": ["brown", "red"]
+    },
+]
