4dae0ab37d50c24ad07334887be26e92e8e85151
Kyla Sauber <kylafaithsauber@gmail.com>
2026-03-23 13:45:46 -0400

    3/23/26

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 prefixes = [
-    {"name": "acorn", 
-    "traits": ["nervous", "childish", "insecure", "calm", "careful", "faithful", "loving", "oblivious"]
-    "colors": ["brown", "red"]
+    {
+        "name": "acorn", 
+        "traits": ["nervous", "childish", "insecure", "calm", "careful", "faithful", "loving", "oblivious"],
+        "colors": ["brown", "red"]
+    },
+    {
+        "name": "adder",
+        "traits": ["troublesome", "fierce", "bloodthirsty", "cold", "daring", "ambitious", "confident", "sneaky", "vengeful", "arrogant", "cunning", "rebellious"],
+        "colors": ["any"]
+    }, 
+    {
+        "name": "alder",
+        "traits": ["lonesome", "fierce", "cold", "nervous", "righteous", "insecure", "strict", "compassionate", "thoughtful", "ambitious",  
+                   "calm", "careful", "faithful", "loving", "loyal", "responsible", "strange", "wise", 
+                   "grumpy", "cunning", "oblivious", "gloomy", "sincere", "flamboyant", "rebellious"],
+        "colors": ["any", "red", "ginger", "orange", "silver", "grey", "blue", "brown", "black", "white", "cream", "yellow", "lilac", "tortoiseshell"]
     },
 ]
+{
+        "name": "",
+        "traits": ["troublesome", "lonesome", "fierce", "bloodthirsty", "cold", "childish", "playful", "charismatic", "bold", "daring", 
+                   "nervous", "righteous", "insecure", "strict", "compassionate", "thoughtful", "ambitious", "confident", "adventurous", 
+                   "calm", "careful", "faithful", "loving", "loyal", "responsible", "shameless", "strange", "sneaky", "vengeful", "wise", 
+                   "arrogant", "competitive", "grumpy", "cunning", "oblivious", "gloomy", "sincere", "flamboyant", "rebellious"],
+        "colors": ["any", "red", "ginger", "orange", "silver", "grey", "blue", "brown", "black", "white", "cream", "yellow", "lilac", "tortoiseshell"]
+    },
